41db2017df622492d964936218f681402db48115
Dian <Dian@DESKTOP-3A2N0I2>
2024-04-20 18:03:35 +0300

    Initial commit

---
+++ src/test/java/thread/com/ApplicationRunnable.java
@@ -0,0 +1,29 @@
+package thread.com;
+
+class RunnerRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello: " + i + " Thread: " + Thread.currentThread().getId());
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ApplicationRunnable {
+
+    public static void main(String[] args) {
+    	
+        Thread thread1 = new Thread(new RunnerRunnable());
+        Thread thread2 = new Thread(new RunnerRunnable());
+        
+        thread1.start();
+        thread2.start();
+    }
+}
